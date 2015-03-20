3cb10b3ad02d509dbc3f0528952c3843ea93597e
Artokun <art.longbottom.jr@gmail.com>
2015-03-19 19:48:27 -0700

    initial gulp configuration for minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,16 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   //no tasks
 });
+gulp.task('copy-html-files', function() {
+  gulp.src(['./app/**/*.html', '!./app/index.html'], {base: './app'})
+  .pipe(gulp.dest('build/'));
+});
+gulp.task('usemin', function() {
+  gulp.src('./app/index.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat', rev()],
+      js: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest('build/'));
+});
+gulp.task('build', ['copy-html-files', 'usemin']);
