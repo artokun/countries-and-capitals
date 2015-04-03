64e9339b0038dcf1824f34321fd77a2a1b4c4e4a
Artokun <art.longbottom.jr@gmail.com>
2015-04-02 19:56:11 -0700

    refactored controllers and services
    Following instruction and guidance from Neville

--- app/components/controllers.js
+++ app/components/controllers.js
@@ -3,22 +3,24 @@ angular.module('myApp.CtrlModule', ['myApp', 'myApp.ServicesModule'])
   .controller('homeCtrl', ['$scope', function ($scope) {
     "use strict";
   }])
-  .controller('countriesCtrl', ['$scope', '$location', 'countriesRequest', 'countryFactory', 'myCache', function ($scope, $location, countriesRequest, countryFactory, myCache) {
+  .controller('countriesCtrl', ['$scope', '$location', 'countryFactory', 'myCache', function ($scope, $location, countryFactory, myCache) {
     "use strict";
+    //Click on row to redirect to detailed country data
     $scope.goToPage = function (countryCode) {
       $location.path('/countries/' + countryCode + '/capital');
     };
-    
+    //Get all countries data from AJAX or cache
     countryFactory.getAllCountries()
       .then(function (data) {
-          $scope.countries = data.geonames;
+        $scope.countries = data.geonames;
       });
   }])
-  .controller('countryCtrl', ['$scope', '$rootScope', 'countryRequest', 'countryFactory', 'capitalRequest', 'neighborsRequest', '$routeParams', function ($scope, $rootScope, countryRequest, countryFactory, capitalRequest, neighborsRequest, $routeParams) {
+  .controller('countryCtrl', ['$scope', '$rootScope', 'countryFactory', '$routeParams', function ($scope, $rootScope, countryFactory, $routeParams) {
     "use strict";
+    //Begin Loading
     $rootScope.isLoading = true;
     var countryCode = $routeParams.country;
-
+    //Get country data
     countryFactory.getCountry(countryCode)
       .then(function (data) {
         var capital,
@@ -26,12 +28,17 @@ angular.module('myApp.CtrlModule', ['myApp', 'myApp.ServicesModule'])
         $scope.country = data.geonames[0];
         capital = data.geonames[0].capital;
         geoId = data.geonames[0].geonameId;
-        capitalRequest(capital, countryCode).success(function (data) {
-          $scope.capital = data.geonames[0];
-        });
-        neighborsRequest(geoId).success(function (data) {
-          $scope.neighbors = data.geonames;
-          $rootScope.isLoading = false;
-        });
+        //Get capital data
+        countryFactory.getCapital(capital, countryCode)
+          .then(function (data) {
+            $scope.capital = data.geonames[0];
+          });
+        //Get neighbor data
+        countryFactory.getNeighbors(geoId)
+          .then(function (data) {
+            $scope.neighbors = data.geonames;
+            //End Loading
+            $rootScope.isLoading = false;
+          });
       });
   }]);
--- app/components/services.js
+++ app/components/services.js
@@ -4,57 +4,47 @@ angular.module('myApp.ServicesModule', ['myApp'])
     "use strict";
     return $cacheFactory('myData');
   })
-  .factory('countryFactory', ['$http','$q','CAC_API_PREFIX','CAC_API_USER','myCache',
-      function($http, $q, CAC_API_PREFIX, CAC_API_USER, myCache) {
+  .factory('countryFactory', ['$http', '$q', 'CAC_API_PREFIX', 'CAC_API_SEARCH', 'CAC_API_USER', 'CAC_API_NEIGHBORS', 'myCache',
+      function ($http, $q, CAC_API_PREFIX, CAC_API_SEARCH, CAC_API_USER, CAC_API_NEIGHBORS, myCache) {
+      "use strict";
+      return {
+        getAllCountries: function () {
+          var cachedData = myCache.get('myData'),
+            deferred;
+          if (cachedData != null) {
+            deferred = $q.defer();
+            deferred.resolve(cachedData);
+            return deferred.promise;
 
-        return {
-          getAllCountries: function() {
-            var cachedData = myCache.get('myData');
-            if (cachedData != null) {
-              var deferred = $q.defer();
-              deferred.resolve(cachedData);
-              return deferred.promise;
+          } else {
 
-            } else {
-
-              return $http.get(CAC_API_PREFIX + CAC_API_USER)
-                        .then(function(data) {
-                            myCache.put('myData', data.data);
-                            return data.data;
-                        });
-            }
-          },
-
-          getCountry: function(countryCode) {
-            return $http.get(CAC_API_PREFIX + 'country=' + countryCode + '&' + CAC_API_USER)
-                      .then(function(data) {
-                        return data.data;
-                      })
+            return $http.get(CAC_API_PREFIX + CAC_API_USER)
+              .then(function (data) {
+                myCache.put('myData', data.data);
+                return data.data;
+              });
           }
-        };
-  }])
+        },
 
-  .factory('countryRequest', ['$http', 'CAC_API_PREFIX', 'CAC_API_USER', function ($http, CAC_API_PREFIX, CAC_API_USER) {
-    "use strict";
-    return function (countryCode) {
-      return $http.get(CAC_API_PREFIX + 'country=' + countryCode + '&' + CAC_API_USER);
-    };
-  }])
-  .factory('capitalRequest', ['$http', 'CAC_API_SEARCH', 'CAC_API_USER', function ($http, CAC_API_SEARCH, CAC_API_USER) {
-    "use strict";
-    return function (capital, countryCode) {
-      return $http.get(CAC_API_SEARCH + capital + '&' + 'country=' + countryCode + '&maxRows=1&style=FULL&' + CAC_API_USER);
-    };
-  }])
-  .factory('neighborsRequest', ['$http', 'CAC_API_NEIGHBORS', 'CAC_API_USER', function ($http, CAC_API_NEIGHBORS, CAC_API_USER) {
-    "use strict";
-    return function (geoId) {
-      return $http.get(CAC_API_NEIGHBORS + geoId + '&' + CAC_API_USER);
-    };
-  }])
-  .factory('countriesRequest', ['$http', 'CAC_API_PREFIX', 'CAC_API_USER', function ($http, CAC_API_PREFIX, CAC_API_USER) {
-    "use strict";
-    return function () {
-      return $http.get(CAC_API_PREFIX + CAC_API_USER);
-    };
-  }]);
+        getCountry: function (countryCode) {
+          return $http.get(CAC_API_PREFIX + 'country=' + countryCode + '&' + CAC_API_USER)
+            .then(function (data) {
+              return data.data;
+            });
+        },
+
+        getNeighbors: function (geoId) {
+          return $http.get(CAC_API_NEIGHBORS + geoId + '&' + CAC_API_USER)
+            .then(function (data) {
+              return data.data;
+            });
+        },
+
+        getCapital: function (capital, countryCode) {
+          return $http.get(CAC_API_SEARCH + capital + '&' + 'country=' + countryCode + '&maxRows=1&style=FULL&' + CAC_API_USER)
+            .then(function (data) {
+              return data.data;
+            });
+        }
+      };
+    }]);
